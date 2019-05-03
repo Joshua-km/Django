34ec03737efca84e2af27a15156cdd5c5fb6c728
joshua <joshua12345_@hotmail.com>
2019-05-03 09:03:24 +0100

    test

---
+++ mysite/wiki/tests.py
@@ -0,0 +1,18 @@
+
+from django.test import TestCase
+
+import datetime
+
+from django.urls import reverse
+
+from django.test import TestCase
+
+from django.utils import timezone
+
+
+# Create your tests here.
+class test(TestCase):
+    
+    def test_no_questions(self):
+        response = self.client.get(reverse('wiki:index'))
+        self.assertContains(response, "No pages are available")
