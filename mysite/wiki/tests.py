e9a701fc0698ce93259453a15ab072c323119318
joshua <joshua12345_@hotmail.com>
2019-05-03 14:57:55 +0100

    help

--- mysite/wiki/tests.py
+++ mysite/wiki/tests.py
@@ -16,3 +16,8 @@ class test(TestCase):
     def test_no_questions(self):
         response = self.client.get(reverse('wiki:index'))
         self.assertContains(response, "No pages are available")
+class test1(TestCase):
+
+    def test_no_questions(self):
+        response = self.client.get(reverse('wiki:index'))
+        self.assertContains(response, "Wiki index")
