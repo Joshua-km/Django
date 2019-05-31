00c461f1111be08f95e7f7e72a6ce53cb4d0d762
joshua <joshua12345_@hotmail.com>
2019-05-31 11:16:24 +0100

    CSS & comments

--- mysite/wiki/tests.py
+++ mysite/wiki/tests.py
@@ -1,42 +1,50 @@
+# This import is need to carry out any test
 from django.test import TestCase
 from django.urls import reverse
 from wiki.models import Page
-from django.contrib.auth.models import User # This is import enables me to test the login & logout syteame
+# This is import enables me to test the login & logout syteame
+from django.contrib.auth.models import User
 
-class Test1(TestCase):#This is testing to check that there is a syste in place if there is no page.
+
+# This is testing to check that there is a syste in place if there is no page.
+class Test1(TestCase):
     def testreplay(self):
         response = self.client.get(reverse('wiki:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No pages are available")
 
-class Test2(TestCase):# This is making sure that there is a title on the page.
+
+# This is making sure that there is a title on the page.
+class Test2(TestCase):
     def testreplay(self):
         response = self.client.get(reverse('wiki:index'))
-        self.assertContains(response, "Wiki Index") 
+        self.assertContains(response, "Wiki Index")
 
-class Test3(TestCase):# This is test to see if there a way that the user can login to the Wiki
+
+class Test3(TestCase):  # This is test to see if there a way that the user can login to the Wiki
     def testreplay(self):
         response = self.client.get(reverse('wiki:index'))
         self.assertContains(response, "login")
 
-class Test4(TestCase):# This is test to see if there a way that the user can logout of the Wiki
+
+class Test4(TestCase):  # This is test to see if there a way that the user can logout of the Wiki
     def testreplay(self):
         response = self.client.get(reverse('wiki:index'))
         self.assertContains(response, "logout")
 
 
-
 class Test5(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user('TestUser', 'TestUser@mywiki.com', 'TestUserpassword')
+        self.user = User.objects.create_user(
+            'TestUser', 'TestUser@mywiki.com', 'TestUserpassword')
 
-    def testLogin(self):  #This is testing to make sure that the login works
+    def testLogin(self):  # This is testing to make sure that the login works
         self.client.login(username='TestUser', password='TestUserpassword')
         response = self.client.get(reverse('wiki:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "logout")
 
-    def testLogout(self): #This is testing to make sure that the logout works
+    def testLogout(self):  # This is testing to make sure that the logout works
         self.client.logout()
         response = self.client.get(reverse('wiki:logout'), follow=True)
         self.assertEqual(response.status_code, 200)
@@ -45,27 +53,16 @@ class Test5(TestCase):
 
 class Test6(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user('TestUser', 'TestUser@gmail.com', 'testingpassword')
+        self.user = User.objects.create_user(
+            'TestUser', 'TestUser@gmail.com', 'testingpassword')
 
-    def testreplay(self): # This test is testing to make sure that the upload page name is in the URL
+    def testreplay(self):  # This test is testing to make sure that the upload page name is in the URL
         self.client.login(username='TestUser', password='testingpassword')
         response = self.client.get('/wiki/upload', follow=True)
         self.assertContains(response, "upload")
 
-    def test_edit_page(self): # This test is testing to make sure that the edit page name is in the URL
+    # This test is testing to make sure that the edit page name is in the URL
+    def test_edit_page(self):
         self.client.login(username='TestUser', password='testingpassword')
         response = self.client.post('/wiki/editing/edit', follow=True)
         self.assertContains(response, 'editing')
-
-
-
-
-
-
-
-
-
-
-
-
-
